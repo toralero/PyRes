8f0ba9985cf74c1bf78eba81895e1f3bb3addecd
Noel Saldanha <2033868162@qq.com>
2020-07-23 08:55:50 +0800

    Added challenge solution

---
+++ 2/prev_challenges/suminputs.py
@@ -0,0 +1,14 @@
+# We can convert the "input()" from a string to an integer by doing casting
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+
+print()
+print("If we check the types now, we can see that both of them are now integers")
+print("type(n1) =", type(n1))
+print("type(n2) =", type(n2))
+print()
+
+total = n1 + n2
+
+print("n1 + n2 =", total)
+
